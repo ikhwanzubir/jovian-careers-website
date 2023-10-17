96b29b045cbc98d98a949e91224cbabca8b52aab
ikhwanzubir <52627770+ikhwanzubir@users.noreply.github.com>
2023-10-17 02:36:14 +0000

    added Requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operator = request.form['operator']
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = "Cannot divide by zero."
+        return render_template('index.html', result=result)
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
